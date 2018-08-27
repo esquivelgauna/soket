8653cb1bbbb08d63e8754866fc810edbc4a59e50
Ricardo Aaron Esquivel <rick>
2018-08-27 12:20:54 -0500

    123

--- application/heplers/database.js
+++ application/heplers/database.js
@@ -1,7 +1,7 @@
 
 var query_builder = require('query_builder');
 var dbconn_default = {
-	host: "http://niurons.com.mx",
+	host: "niurons.com.mx",
 	user: "niuronsc_Dev",
 	pass: "**niuronsdev2017",
 	dbase: "niuronsc_db_niurons"
